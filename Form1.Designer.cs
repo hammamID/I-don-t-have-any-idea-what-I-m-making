acd727075171fca673a48c46e7828c51a74e2209
hammamID <101692420+hammamID@users.noreply.github.com>
2024-10-08 20:19:44 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace ClockOpp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label lblClock;
+        private System.Windows.Forms.Label lblDate;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.lblClock = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblClock
+            // 
+            this.lblClock.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblClock.AutoSize = true;
+            this.lblClock.Font = new System.Drawing.Font("Arial", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClock.ForeColor = System.Drawing.Color.White;
+            this.lblClock.Location = new System.Drawing.Point(12, 11);
+            this.lblClock.Name = "lblClock";
+            this.lblClock.Size = new System.Drawing.Size(503, 111);
+            this.lblClock.TabIndex = 0;
+            this.lblClock.Text = "Loading...";
+            this.lblClock.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDate
+            // 
+            this.lblDate.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblDate.AutoSize = true;
+            this.lblDate.Font = new System.Drawing.Font("Arial", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDate.ForeColor = System.Drawing.Color.White;
+            this.lblDate.Location = new System.Drawing.Point(12, 134);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(503, 111);
+            this.lblDate.TabIndex = 1;
+            this.lblDate.Text = "Loading...";
+            this.lblDate.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblClock);
+            this.Name = "Form1";
+            this.Text = "Clock";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Timers;
+
+namespace ClockOpp
+{
+    public partial class Form1 : Form
+    {
+        private System.Timers.Timer timer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            StartClock();
+        }
+
+        private void StartClock()
+        {
+            timer = new System.Timers.Timer(1000); // Update every second
+            timer.Elapsed += UpdateClock;
+            timer.Start();
+        }
+
+        private void UpdateClock(object sender, ElapsedEventArgs e)
+        {
+            // Update the clock and date labels on the UI thread
+            Invoke(new Action(() =>
+            {
+                lblClock.Text = DateTime.Now.ToString("HH:mm:ss");
+                lblDate.Text = DateTime.Now.ToString("MMMM dd, yyyy");
+            }));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
